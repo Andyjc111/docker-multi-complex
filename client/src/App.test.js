4fc7c859844e38b950144c46b732e2d4430fc2ba
Andy <andyjc111@gmail.com>
2022-07-04 16:17:10 +1000

    fixing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,14 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test(console.log('fuck this test'));
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
